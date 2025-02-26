d1ee80055844cea3ba86af09e134026c37f38cc9
msanya2169 <msanya2169@inst.hcpss.org>
2025-02-26 15:04:09 +0000

    2/26

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,1 +1,14 @@
 package org.example; //DO NOT DELETE
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {    
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number of gallons: ");
+        double gallons = input.nextInt();
+        System.out.println("In " + gallons + " gallons there are: " );
+        System.out.println((gallons*4) + " quarts");
+        System.out.println((gallons*4)*2 + " pints");
+        System.out.println(((gallons*4)*2)*2 + " cups");
+        System.out.println((((gallons*4)*2)*2)*16 + " tablespoons");
+    }
+}
